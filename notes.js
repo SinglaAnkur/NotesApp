668bd077038bfe94661d4ac27899989adb773a35
unknown <singla.ankur147@gmail.com>
2020-07-23 19:19:56 -0400

    Read note function and minor code changes

--- notes.js
+++ notes.js
@@ -1,16 +1,11 @@
 const fs = require('fs')
 const chalk = require('chalk')
-const getNotes = () => {
-    return "Your Notes"
-}
-
 
 const addNote = (title, body) => {
     const notes = loadNotes()
+    const duplicateNote = notes.find((note) => note.title === title)
 
-    const duplicateNotes = notes.filter((note) => note.title === title)
-
-    if (duplicateNotes.length === 0) {
+    if (!duplicateNote) {
         notes.push({
             title:title,
             body: body
@@ -60,9 +55,22 @@ const listNotes = () => {
     })
 }
 
+const readNotes = (title) => {
+    const notes = loadNotes()
+    const note = notes.find((note) => note.title === title)
+
+    if(note) {
+        console.log(chalk.inverse(note.title))
+        console.log(note.body)
+    }
+    else {
+        console.log(chalk.red.inverse("No note found!"))
+    }
+}
+
 module.exports = {
-    getNotes: getNotes,
     addNote: addNote,
     removeNote: removeNote,
-    listNotes: listNotes
+    listNotes: listNotes,
+    readNotes: readNotes
 }
